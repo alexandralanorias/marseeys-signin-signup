6a380fce3c4060540ab8f67244e862303c1777ec
alexandra <97576137+alexandralanorias@users.noreply.github.com>
2022-10-13 23:39:37 -0400

    Initial signin/signup setup

--- marseeys-signup-signin/src/App.js
+++ marseeys-signup-signin/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import ContainerWelcome from './components/ContainerWelcome';
 
-// importing login components
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// importing login component
 import Auth from "./components/Auth";
 
 // importing bootstrap
@@ -11,12 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
     <div className="App">
-        {/* <ContainerWelcome/> */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-          </Routes>
-        </BrowserRouter>
+        <Auth/>
     </div>
   );
 }
