7f9312b46f1cd9425d05026d921077649c43c750
alexandra <97576137+alexandralanorias@users.noreply.github.com>
2022-10-14 03:31:05 -0400

    'Forgot password' modal added

--- marseeys-signup-signin/src/components/Auth.jsx
+++ marseeys-signup-signin/src/components/Auth.jsx
@@ -1,6 +1,62 @@
 import React, { useState } from "react"
 
+// importing react-bootstrap checkbox
+import { Checkbox } from 'react-bootstrap';
+
+// importing button and modal from react-bootstrap
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+// function for modal
+function VerticallyCenteredModal(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  return (
+    <Modal
+      {...props}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Enter your username
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="no spaces, e.g. jsbach"
+            autoFocus
+          />
+        </Form.Group>
+      </Form>
+      <p>
+        Make sure that you are typing an existing username.
+      </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Submit
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 export default function (props) {
+  // show modal
+  const [modalShow, setModalShow] = React.useState(false);
+
   let [authMode, setAuthMode] = useState("signin")
 
   const changeAuthMode = () => {
@@ -40,6 +96,13 @@ export default function (props) {
                 Submit
               </button>
             </div>
+            <Button className="mt-3" variant="warning" onClick={() => setModalShow(true)}>
+              Forgot password?
+            </Button>
+            <VerticallyCenteredModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />
           </div>
         </form>
       </div>
@@ -81,11 +144,35 @@ export default function (props) {
               placeholder="Password"
             />
           </div>
+          <div className="form-group mt-3">
+            <label>Roles</label>
+          </div>
+          <div class="form-check mt-3">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+            <label class="form-check-label" for="flexCheckDefault">
+              Admin
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+            <label class="form-check-label" for="flexCheckDefault">
+              Mod
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
+            <label class="form-check-label" for="flexCheckChecked">
+              User
+            </label>
+          </div>
           <div className="d-grid gap-2 mt-3">
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
         </div>
       </form>
     </div>
